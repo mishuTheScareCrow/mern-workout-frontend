8f029fc845955233891d67de06cd296386a2bb6b
Abdur Rahman <bmishu33@gmail.com>
2024-06-04 01:54:37 -0700

    Added delete button to remove workouts

--- src/components/WorkoutDetails.jsx
+++ src/components/WorkoutDetails.jsx
@@ -1,4 +1,14 @@
-export default function WorkoutDetails({ title, load, reputation, createdAt }) {
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+export default function WorkoutDetails({ _id, title, load, reputation, createdAt }) {
+  const { dispatch } = useWorkoutsContext();
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + _id, { method: "DELETE" });
+
+    const json = await response.json();
+
+    if (response.ok) dispatch({ type: "DELETE_WORKOUT", payload: json });
+  };
   return (
     <div className="workout-details">
       <h4>{title}</h4>
@@ -11,6 +21,7 @@ export default function WorkoutDetails({ title, load, reputation, createdAt }) {
         {reputation}
       </p>
       <p>{createdAt}</p>
+      <button onClick={handleClick}>Delete</button>
     </div>
   );
 }
